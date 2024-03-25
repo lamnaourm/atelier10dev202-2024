ae24c951e7c58484f5e2711ae73a706d5d335f7f
lamnaourm <mohammed.lamnaour@gmail.com>
2024-03-25 14:39:49 +0000

    product service

---
+++ product-service/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import productroute from './routes/product.js'
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/dbproducts')
+        .then(() => {
+            console.log('Connected to mongodb')
+        })
+        .catch((err) => {
+            console.log('Unable to connect to mongodb')
+        })
+
+app.use('/products', productroute)
+
+app.listen(3000, (err) => {
+    if(!err)
+        console.log('Unable to start Server at 3000')
+    else 
+        console.log('Server Started')
+})
---
+++ product-service/routes/product.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const routes = express.routes
+
+routes.post('/', (req, res) => {
+
+})
+
+routes.post('/buy', (req, res) => {
+    
+})
+
+export default routes
