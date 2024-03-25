16a758b6d7252fd07179783b46e3fe1ab0b6d3d0
lamnaourm <mohammed.lamnaour@gmail.com>
2024-03-25 15:32:27 +0000

    route 1

--- product-service/routes/product.js
+++ product-service/routes/product.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import productModel from '../models/Product.js'
 
-const routes = express.routes
+const routes = express.Router()
 
 routes.post('/', (req, res) => {
 
+    const product = req.body;
+
+    productModel.create(product)
+        .then((p) => {
+            return res.json(p)
+        })
+        .catch((err) => {
+            return res.status(510).send('erreur creation product')
+        })
 })
 
 routes.post('/buy', (req, res) => {
