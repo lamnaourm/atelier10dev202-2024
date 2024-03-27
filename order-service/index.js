a6c2ff21f4c8aa1df1f1517d4972074de1de8dce
lamnaourm <mohammed.lamnaour@gmail.com>
2024-03-27 13:41:30 +0000

    solution

---
+++ order-service/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import amqp from 'amqplib'
+import OrderModel from './models/Order.js'
+
+const app = express();
+app.use(express.json());
+
+var connection, channel;
+const queueName1 = 'order-service-queue';
+const queueName2 = 'produit-service-queue';
+
+mongoose.connect('mongodb://localhost:27017/dborders')
+    .then(() => {
+        console.log('Connected to mongodb')
+    })
+    .catch((err) => {
+        console.log('Unable to connect to mongodb')
+    })
+
+async function connectToRabbitMQ() {
+    const amqpServer = "amqp://guest:guest@localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue(queueName1);
+    await channel.assertQueue(queueName2);
+}
+
+connectToRabbitMQ().then(() => {
+    channel.consume(queueName1, (data) => {
+        const products = JSON.parse(data.content.toString());
+        console.log(products)
+        const total = products.reduce((som,p) => som+p.price, 0)
+        const order = {products, total}
+
+        OrderModel.create(order).then((o) => {
+           channel.sendToQueue(queueName2, Buffer.from(JSON.stringify(o)))
+        })
+
+        channel.ack(data)
+    })
+})
+
+app.listen(3001, (err) => {
+    if (!err)
+        console.log('Unable to start Server at 3000')
+    else
+        console.log('Server Started')
+})
--- product-service/routes/product.js
+++ product-service/routes/product.js
@@ -1,7 +1,24 @@
 import express from 'express'
+import amqp from 'amqplib'
 import productModel from '../models/Product.js'
 
 const routes = express.Router()
+
+var connection, channel;
+const queueName1='order-service-queue';
+const queueName2='produit-service-queue';
+
+async function connectToRabbitMQ() {
+    const amqpServer = "amqp://guest:guest@localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue(queueName1);
+    await channel.assertQueue(queueName2);
+ }
+
+ connectToRabbitMQ().then(() => {
+    console.log('Connected to Rabbit MQ')
+ })
 
 routes.post('/', (req, res) => {
 
@@ -17,7 +34,21 @@ routes.post('/', (req, res) => {
 })
 
 routes.post('/buy', (req, res) => {
-    
+    const list = req.body;
+
+    productModel.find({'_id':{$in:list}})
+        .then((products) => {
+            channel.sendToQueue(queueName1, Buffer.from(JSON.stringify(products)))
+
+            channel.consume(queueName2, (data) => {
+                res.json(JSON.parse(data.content.toString()))
+
+                channel.ack(data)
+            })
+        })
+        .catch((err) => {
+            return res.status(510).send('erreur' + err)
+        })
 })
 
 export default routes
